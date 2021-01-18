d09124e89c00d01af41a537b04d711cd2e176018
sinmo928 <71582880+sinmo928@users.noreply.github.com>
2021-01-18 17:00:59 +0900

    new-project test

--- 성인모/test_2.py
+++ 성인모/test_2.py
@@ -1,1 +1,9 @@
-#second test
+word = input('단어를 입력하시오')
+n = len(word)
+for i in range(n//2):
+    if word[i] == word[-1-i]:
+        print('회문입니다')
+        break
+    else:
+        print('회문이 아닙니다')
+        break
