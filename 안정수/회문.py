97ae2c9039bb2cdf3bf146aed6e8f61fd57b5d8e
carina1754 <bulebed1754@gmail.com>
2021-01-19 22:44:58 +0900

    2021-01-19-22:44

--- 안정수/회문.py
+++ 안정수/회문.py
@@ -1,11 +1,8 @@
 word = input('input word : ')
-flag = 0
-for i in range(int(len(word)/2)):
-    if word[i]==word[-i-1]:
-        flag = 1
-    else:
-        flag = 0
-if flag == 1:
-    print("회문")
+
+for i in range(len(word)//2):
+    if word[i] != word[-i-1]:
+        print("회문아님")
+        break
 else:
-    print("회문아님")
+    print("회문")
