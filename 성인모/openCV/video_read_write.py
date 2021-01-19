8d4bf105eac1a4777ed7da21181d0f33d878872e
sinmo928 <71582880+sinmo928@users.noreply.github.com>
2021-01-19 11:54:09 +0900

    openCV draw study

--- 성인모/openCV/video_read_write.py
+++ 성인모/openCV/video_read_write.py
@@ -31,4 +31,40 @@ def showVideo():
     cv2.destroyAllWindows()
 
 
-showVideo()
+def writeVideo():
+    try:
+        print('카메라를 구동합니다.')
+        cap = cv2.VideoCapture(1)
+    except:
+        print('카메라 구동실패')
+        return
+
+    fps = 20.0
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+    fcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')
+
+    out = cv2.VideoWriter('images/mycam.avi', fcc, fps, (width, height))
+    print('녹화 시작')
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            print('비디오 읽기 오류')
+            break
+
+        cv2.imshow('video', frame)
+        out.write(frame)
+
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            print('녹화 종료')
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+# showVideo()
+writeVideo()
